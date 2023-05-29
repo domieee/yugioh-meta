63196199fcd2f4607c1c47b767f659b5fe063ed6
domieee <gartzdominik@gmail.com>
2023-05-29 14:08:44 +0200

    :heavy_plus_sign: in auth/register: added inputs and state logic and functionality for axios fetch

--- app/(site)/register/page.jsx
+++ app/(site)/register/page.jsx
@@ -1,5 +1,108 @@
+'use client'
+
+import { useState } from "react";
+import axios from "axios"
+import CircularProgress from '@mui/material/CircularProgress';
+import { useRouter } from "next/navigation";
+import Link from "next/link"
+// import './register.scss'
+
+
 export default function Register() {
+
+    const router = useRouter()
+
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const [fetching, setFetching] = useState(false);
+
+    const registerUser = async (e) => {
+        e.preventDefault()
+        setFetching(true)
+        axios.post('/api/register', data)
+
+            .then((res) => {
+                console.log(res)
+                console.log('Registration successful')
+                router.replace('/')
+            })
+
+            .catch((err) => {
+                setFetching(false)
+                console.log('Registration failed')
+                console.log(err)
+            })
+    }
+
     return (
-        <h1>Register</h1>
+        <section className="registerPage">
+
+            {fetching ?
+                <div className="registerForm">
+                    <CircularProgress color="success" />
+                </div>
+                :
+                <form
+                    className="registerForm"
+                    onSubmit={registerUser}>
+                    <article className="formRow">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            id="username"
+                            placeholder="johndoe1337"
+                            required
+                            value={data.name}
+                            onChange={e => setData({ ...data, name: e.target.value })} />
+                    </article>
+
+                    <article className="formRow">
+                        <label htmlFor="email">E-Mail</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="john@doe.com"
+                            required
+                            value={data.email}
+                            onChange={e => setData({ ...data, email: e.target.value })} />
+                    </article>
+
+                    <article className="formRow">
+                        <label htmlFor="email">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="Password"
+                            required
+                            value={data.password}
+                            onChange={e => setData({ ...data, password: e.target.value })} />
+                    </article>
+
+                    <article className="formRow">
+                        <label htmlFor="email">Confirm Password</label>
+                        <input
+                            type="password"
+                            name="confirmation"
+                            id="confirmation"
+                            placeholder="Confirm Password"
+                            required
+                            value={data.confirmPassword}
+                            onChange={e => setData({ ...data, confirmPassword: e.target.value })} />
+                    </article>
+
+                    <button type="submit">Register Account</button>
+
+                    <p>Already a member? <Link href='/login'>Sign in</Link></p>
+                </form>}
+
+        </section>
     )
 }
