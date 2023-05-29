e5bfb90ce81deb84f8e32e34fb488bf234c54708
domieee <gartzdominik@gmail.com>
2023-05-29 13:54:51 +0200

    :heavy_plus_sign: change: dynamic page title for better indication of development and production tab

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(authOptions)
+
   return (
     <main>
       <p>Home</p>
+      <h1>Server Side Rendered</h1>
+      <pre>{JSON.stringify(session)}</pre>
     </main>
   )
 }
