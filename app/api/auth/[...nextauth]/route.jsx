b8bda6c0fd0ec724fd294cffd0eb840e6363ec9f
domieee <gartzdominik@gmail.com>
2023-05-29 12:52:11 +0200

    :heavy_plus_sign: add: credentials provider for prisma

---
+++ app/api/auth/[...nextauth]/route.jsx
@@ -0,0 +1,27 @@
+import NextAuth from 'next-auth/next'
+import prisma from '../../../libs/prismadb'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import CredentialsProovider from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientID: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
+        }),
+        CredentialsProovider({
+            name: "credentials",
+            credentials: {
+                name: { label: 'Name', type: 'name', placeholder: 'Name' },
+                email: { label: 'Email', type: 'email', placeholder: 'Email' },
+                password: { label: 'Password', type: 'password', placeholder: 'Password' }
+            },
+            async authorize(credentials) {
+                const user = { id: 1, name: 'JohnDoe', email: 'johndoe@mail.com' }
+                return user
+            }
+        })
+    ]
+}
