248190a178bcf4abd487cf7e7253513b619eecf7
domieee <gartzdominik@gmail.com>
2023-05-30 04:11:56 +0200

    :heavy_plus_sign: change: in components/navigation: position to sticky

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,9 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
       <body className={inter.className}>
         <NextAuthProvider>
           <Navigation />
