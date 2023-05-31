b96d3fd754f17bbd10d6182c3f3a3f603c86179c
domieee <gartzdominik@gmail.com>
2023-05-31 16:26:51 +0200

    :rocket: mui components

--- app/statistics/components/TablePie.jsx
+++ app/statistics/components/TablePie.jsx
@@ -6,18 +6,34 @@ import PieChart from "./PieChart"
 import TableMUI from "./TabelMUI"
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import { createTheme } from '@mui/material/styles';
 
 export default function TablePie({ winnerJson, topCutJson }) {
+
+    const theme = createTheme({
+        breakpoints: {
+            values: {
+                xxs: 0, // small phone
+                xs: 300, // phone
+                sm: 600, // tablets
+                md: 900, // small laptop
+                lg: 1200, // desktop
+                xl: 1536 // large screens
+            }
+        }
+    });
     return (
         <Box
+
             display="flex"
             flexDirection="column"
-            bgcolor="background.paper"
+
             justifyContent='center'
             p={2}
             borderRadius={4}
             boxShadow={8}
             marginBottom={2}
+
         >
             <Typography
                 paddingLeft={10}
@@ -26,14 +42,20 @@ export default function TablePie({ winnerJson, topCutJson }) {
                 {winnerJson ? "Winner Breakdown" : " Overall Top Cut Breakdown"}
             </Typography>
             <Box
+                theme={theme}
                 justifyContent='space-evenly'
                 display="flex"
                 flexDirection="row"
-                bgcolor="background.paper"
                 p={2}
                 borderRadius={4}
 
-                marginBottom={2}>
+                marginBottom={2}
+                sx={{
+                    flexDirection: {
+                        xs: 'column',
+                        sm: 'column'
+                    }
+                }}>
                 <div className="canvasContainer">
                     <PieChart data={winnerJson || topCutJson} />
                 </div>
--- app/statistics/components/TablePieBox.jsx
+++ app/statistics/components/TablePieBox.jsx
@@ -4,7 +4,10 @@ import React from 'react'
 import TablePie from './TablePie'
 import Box from '@mui/material/Box';
 
+
 export default function TablePieBox({ winnerJson, topCutJson }) {
+
+
     return (
         <>
             <Box
@@ -15,6 +18,7 @@ export default function TablePieBox({ winnerJson, topCutJson }) {
                 borderRadius={4}
                 boxShadow={8}
                 marginBottom={2}
+
             >
                 <TablePie winnerJson={winnerJson} />
                 <TablePie topCutJson={topCutJson} />
