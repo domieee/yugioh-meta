0a7687bdb0ba97cd0d68c966b45bfac63a923022
domieee <gartzdominik@gmail.com>
2023-07-09 17:30:24 +0200

    :heavy_plus_sign: in page.js: added content for the landingpage

--- app/page.js
+++ app/page.js
@@ -10,26 +10,27 @@ export default async function Home() {
 
   console.log(token)
 
+
   return (
     <>
       <section style={{ height: '90vh', position: 'relative', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 
         <article>
-          <Image
+          <Image className='rotation-animation'
             src='/_48937081-5bc2-4567-98ed-6619c20f48de.png'
             alt='Buy me a Coffee Button'
             width={500}
             height={500} />
         </article>
         <article style={{ position: 'relative', marginLeft: '50px', maxWidth: '800px' }}>
-          <h1 variant='h1'>Unleash Your Dueling Potential with Yu-Gi-Oh Meta!</h1>
-          <p>Your Source for Current Tournament Statistics and Insights. Elevate Your Dueling Game with In-Depth Analysis and Winning Strategies.</p>
+          <h1 style={{ marginBottom: '10px' }} variant='h1'>Unleash Your Dueling Potential with Yu-Gi-Oh Meta!</h1>
+          <p style={{ marginBottom: '20px' }}>Your Source for Current Tournament Statistics and Insights. Elevate Your Dueling Game with In-Depth Analysis and Winning Strategies.</p>
           {token ? null : <Link href={`${process.env.NEXTAUTH_URL}/register`}>Sign up your account</Link>}
         </article>
-      </section >
-      <section>
+      </section  >
+      <section style={{ height: '100vh' }}>
 
-      </section>
+      </section >
 
     </>
   )
